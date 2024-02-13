3fa85ee675421f9be3906637b1266b1f0df26c1f
Vadim <zyuskovv@gmail.com>
2024-02-13 03:16:21 +0300

    AppDrawer - ProfileInfo and adding ConstaingLayout 1.0.1

--- app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
+++ app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,6 +25,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.topic3.android.reddit.R
 
 import com.topic3.android.reddit.theme.RedditThemeSettings
@@ -71,6 +73,7 @@ private fun AppDrawerHeader() {
     )
     Text(text = stringResource(id = R.string.default_username),
       color = MaterialTheme.colors.primaryVariant)
+    ProfileInfo()
   }
   Divider(
     color = MaterialTheme.colors.onSurface.copy(alpha = .2f),
@@ -81,8 +84,43 @@ private fun AppDrawerHeader() {
 }
 
 @Composable
-fun ProfileInfo() {
-  //TODO add your code here
+fun ProfileInfo(modifier: Modifier = Modifier) {
+  ConstraintLayout(
+    modifier = modifier
+      .fillMaxWidth()
+      .padding(top = 16.dp)
+  ) {
+    val (karmaItem, divider, ageItem) = createRefs()
+    val colors = MaterialTheme.colors
+
+    ProfileInfoItem(
+      Icons.Filled.Star,
+      R.string.default_karma_amount,
+      R.string.karma,
+      modifier = modifier.constrainAs(karmaItem){
+        centerVerticallyTo(parent)
+        start.linkTo(parent.start)
+      }
+    )
+    Divider(
+      modifier = modifier
+        .width(1.dp)
+        .constrainAs(divider) {
+          centerVerticallyTo(karmaItem)
+          centerHorizontallyTo(parent)
+          height = Dimension.fillToConstraints
+        },
+      color = colors.onSurface.copy(alpha = .2f)
+    )
+    ProfileInfoItem(Icons.Filled.ShoppingCart,
+      R.string.default_reddit_age_amount,
+      R.string.reddit_age,
+      modifier = modifier.constrainAs(ageItem){
+        start.linkTo(divider.end)
+        centerVerticallyTo(parent)
+      }
+    )
+  }
 }
 
 @Composable
@@ -127,7 +165,7 @@ private fun ProfileInfoItem(
       fontSize = 10.sp,
       modifier = itemModifier
         .padding(start = 8.dp)
-        .constrainAs(titleRef){
+        .constrainAs(titleRef) {
           top.linkTo(amountRef.bottom)
           start.linkTo(iconRef.end)
           bottom.linkTo(iconRef.bottom)
